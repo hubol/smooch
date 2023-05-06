0ad2df3d12ef9d8ae29ff4f6e98cea4c1a7788d9
Hubol <hubol.gordon@gmail.com>
2023-05-06 16:14:09 -0500

    Test adding images

--- test/test.ts
+++ test/test.ts
@@ -13,9 +13,19 @@ async function runTest() {
             "folder": "src-images",
             "outFolder": "dst-images",
             "outTemplateExtension": "ts",
+            "pack": {
+                "maxWidth": 1024,
+                "maxHeight": 1024,
+            }
         }]
     });
     const smooch = TestProject.smooch();
+    await smooch.stdOut.untilPrinted('Packed');
+    await TestProject.fixture('image256Png', 'src-images/image0.png');
+    await smooch.stdOut.untilPrinted('Packed');
+    for (let i = 1; i < 10; i++) {
+        await TestProject.fixture('image256Png', `src-images/image${i}.png`);
+    }
     await smooch.stdOut.untilPrinted('Packed');
     smooch.kill();
     await smooch.untilExited();
