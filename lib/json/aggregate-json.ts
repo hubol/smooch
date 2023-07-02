7985abee09cdb3a6dee68273b307add75e083a50
Hubol <hubol.gordon@gmail.com>
2023-07-02 17:47:07 -0500

    Aggregate JSON works with refactor

--- lib/json/aggregate-json.ts
+++ lib/json/aggregate-json.ts
@@ -6,12 +6,24 @@ import { glob } from "glob";
 import { JsonFile } from "../common/json-file";
 import { JsTemplate } from "../common/template";
 import { Logger } from "../common/logger";
+import { SmoochWorkAcceptor } from "../main/pipeline/smooch-work-acceptor";
+import { SmoochWorkPipelineRecipe } from "../main/pipeline/smooch-work-pipeline";
+import { SmoochWorkQueue } from "../main/pipeline/smooch-work-queue";
 
 export const AggregateJsonOptions = object({
     folder: SmoochStruct.CwdRelativePath,
     outFile: SmoochStruct.CwdRelativePath,
     outTemplate: defaulted(SmoochStruct.CwdRelativePath, new CwdRelativePath(Fs.resolve(__filename, '../default-template.js'))),
 });
+
+export const AggregateJsonRecipe: SmoochWorkPipelineRecipe<Infer<typeof AggregateJsonOptions>> = {
+	acceptorFactory: options => {
+		const jsonFolder = Fs.resolve(options.folder.absolutePath, '**/*.json');
+		return new SmoochWorkAcceptor([ jsonFolder ], []);
+	},
+	queueFactory: () => new SmoochWorkQueue(),
+	workFnFactory: options => () => aggregateJson(options),
+}
 
 const logger = new Logger('JsonAggregator', 'cyan');
 
