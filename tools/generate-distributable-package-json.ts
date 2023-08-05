9227cd0f72088f8e92831ed18d0769c945ceebb6
Hubol <hubol.gordon@gmail.com>
2023-08-05 18:19:54 -0500

    Simplify directory structure of distributed code

---
+++ tools/generate-distributable-package-json.ts
@@ -0,0 +1,15 @@
+import { Fs } from "../lib/common/fs";
+import packageJson from "../package.json";
+
+async function main() {
+    const json: Partial<typeof packageJson> = packageJson;
+    delete json.devDependencies;
+    delete json.scripts;
+
+    const string = JSON.stringify(json, undefined, 2);
+    const dstFile = 'dist/package.json';
+    Fs.writeFile(dstFile, string);
+    console.log(`Wrote distributable package.json to ${dstFile}`);
+}
+
+main();
