e501554672181557b10cd3a23ba721bd4282278c
Hubol <hubol.gordon@gmail.com>
2023-08-24 17:10:03 -0500

    Improve template-api type names

--- dist/templates/convert-audio.js
+++ dist/templates/convert-audio.js
@@ -1,3 +1,7 @@
+/**
+@param {import("smooch/template-api").TemplateContext.AudioConvert} context;
+@param {import("smooch/template-api").Utils} utils;
+*/
 module.exports = function ({ files }, { pascal, noext }) {
     return `
 // This file is generated
