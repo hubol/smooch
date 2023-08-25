172ea506adf0aa7fa9268956df2e0f0cd996bdd7
Hubol <hubol.gordon@gmail.com>
2023-08-25 12:53:58 -0500

    Support copying template programs

---
+++ lib/main/cli-utils/commands.ts
@@ -0,0 +1,22 @@
+import { Path } from "../../common/path";
+import { copyTemplateProgram } from "./copy-template-program";
+
+const cliUtilsCommands: Record<string, (...args: string[]) => Promise<unknown>> = {
+    'copy-program': (src, dst) => copyTemplateProgram(src, Path.File.create(dst))
+}
+
+export async function runCliUtilCommand() {
+    const args = process.argv;
+    const commands = Object.keys(cliUtilsCommands);
+    for (const command of commands) {
+        const index = args.indexOf(command);
+        if (index === -1)
+            continue;
+
+        const commandArgs = args.slice(index + 1);
+        await cliUtilsCommands[command](...commandArgs);
+        return true;
+    }
+
+    return false;
+}
---
+++ lib/main/cli-utils/copy-template-program.ts
@@ -0,0 +1,41 @@
+import chalk from "chalk";
+import { describeList } from "../../common/describe-list";
+import { Environment } from "../../common/environment";
+import { Fs } from "../../common/fs";
+import { Gwob } from "../../common/gwob";
+import { Logger } from "../../common/logger";
+import { Path } from "../../common/path";
+
+const logger = new Logger('CopyTemplateProgram', 'green');
+
+export async function copyTemplateProgram(src: string, dstFile: Path.File.t) {
+    const srcFile = await findTemplateProgramFile(src);
+    if (!srcFile)
+        return;
+
+    if (!dstFile)
+        dstFile = Path.File.create(Fs.parse(srcFile).base);
+
+    logger.log(`${srcFile} -> ${dstFile}`);
+    await Fs.copyFile(srcFile, dstFile);
+}
+
+async function findTemplateProgramFile(src: string) {
+    const glob = Path.Glob.create(templateProgramDirectory, '*.js');
+    const files = await Gwob.files(glob);
+    for (const file of files) {
+        const { base } = Fs.parse(file);
+        if (base.includes(src))
+            return file;
+    }
+
+    const list = describeList(files.map(file => Fs.parse(file).name));
+    logger.log(`Could not find template program to copy.
+Available template programs: ${chalk.white(list)}`);
+
+    return null;
+}
+
+const templateProgramDirectory = Environment.isDev
+    ? Fs.resolve(__filename, '../../../../dist/templates')
+    : Fs.resolve(__filename, '../templates');
