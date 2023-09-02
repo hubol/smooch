d79ae71e73e1846a424bba1d595e522c37bb05a4
Hubol <hubol.gordon@gmail.com>
2023-09-02 16:40:56 -0500

    Strip . in configured file extensions

--- lib/common/custom-superstruct.ts
+++ lib/common/custom-superstruct.ts
@@ -4,7 +4,7 @@ import { Path } from "./path";
 const FilePath = coerce(string() as unknown as Struct<Path.File.t, null>, string(), (value) => Path.File.create(value));
 const DirectoryPath = coerce(string() as unknown as Struct<Path.Directory.t, null>, string(), (value) => Path.Directory.create(value));
 const GlobPath = coerce(string() as unknown as Struct<Path.Glob.t, null>, string(), (value) => Path.Glob.create(value));
-const FileExtension = coerce(string(), string(), (value) => value.trim().toLocaleLowerCase('en-US'));
+const FileExtension = coerce(string(), string(), (value) => value.trim().toLocaleLowerCase('en-US').replace(/\./g, ''));
 
 export const SmoochStruct = {
     FilePath,
